7dfe267271e3b0a9b3e653d0054399fb1d7fe84c
distractabee <jssclgh@gmail.com>
2024-03-07 18:27:38 -0600

    adding variables to JavaScript to be able to use API and search feature

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var openWeatherAPIKey = "7fa6d32ee5c95d1a1f2785285f1d8c5c";
-var city;
+var city = document.getElementById("userInput");
+var submitButton = document.getElementById("submit-btn");
+submitButton.addEventListener('click', submitButtonEvent);
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={7fa6d32ee5c95d1a1f2785285f1d8c5c}
+
+function submitButtonEvent(event) {
+    var userCity = city.value;
+    event.preventDefault();
+
+}
